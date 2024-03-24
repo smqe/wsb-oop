5818819929ea58ec88ee412dfd78ea017f2ef355
smqe <35816979+smqe@users.noreply.github.com>
2024-03-25 00:35:52 +0100

    EF and SQLite packages added

--- wsb-oop/Program.cs
+++ wsb-oop/Program.cs
@@ -1,9 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//zmienic na jakas inna nazwe
+var connectionString = builder.Configuration.GetConnectionString("Name") ?? "Data Source=Name.db";
+
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddSqlite<NameDb>(connectionString);
 
 var app = builder.Build();
 
