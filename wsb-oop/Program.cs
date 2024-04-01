9d68e6b6d0bb7b025b7e4be0c49868d1a8fc304e
Błażej Szulik <b.szulik@tuta.io>
2024-04-01 15:04:46 +0200

    add swagger gen

--- wsb-oop/Program.cs
+++ wsb-oop/Program.cs
@@ -1,29 +1,23 @@
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-
-
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
 using Microsoft.EntityFrameworkCore;
 using wsb_oop.Product.db;
 using wsb_oop.Product.services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddSingleton<IProductService, ProductService>();
-builder.Services.AddDbContext<ProductDbContext>(options=>
+var services = builder.Services;
+services.AddControllers();
+services.AddScoped<IProductService, ProductService>();
+services.AddDbContext<ProductDbContext>(options=>
     options.UseSqlite("Data Source=Product.db")
     );
 
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.MapControllers();
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.Run();
